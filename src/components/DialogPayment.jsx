5abdfe3a925a0e2a3df52281f377e46392fc888d
inkbasic <93990792+inkbasic@users.noreply.github.com>
2025-09-13 23:12:26 +0700

    Add Button import to DialogPayment component
    Imported the Button component from ui/button in DialogPayment.jsx, likely to enable usage of Button within this component.

--- src/components/DialogPayment.jsx
+++ src/components/DialogPayment.jsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 import {
     Select,
     SelectContent,
