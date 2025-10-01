39cb51e5e4df39a778cef7b02fb2572106bf7873
OkuSan - Paratpanu Pechsaman <paratpanu18@gmail.com>
2025-10-01 23:07:12 +0700

    ci: centralize public api url on .env

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,51 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 // shadcn
 import path from "path"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwind()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/auth": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-      '/places': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/users': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-      "/plans": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-      "/tags": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-      "/ad": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  console.log("VITE_PUBLIC_API_URL:", process.env.VITE_PUBLIC_API_URL);
+
+  return defineConfig({
+    plugins: [react(), tailwind()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-});
+    server: {
+      proxy: {
+        "/auth": {
+          target: process.env.VITE_PUBLIC_API_URL || 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/places': {
+          target: process.env.VITE_PUBLIC_API_URL || 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/users': {
+          target: process.env.VITE_PUBLIC_API_URL || 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+        },
+        "/plans": {
+          target: process.env.VITE_PUBLIC_API_URL || "http://localhost:3000",
+          changeOrigin: true,
+        },
+        "/tags": {
+          target: process.env.VITE_PUBLIC_API_URL || "http://localhost:3000",
+          changeOrigin: true,
+        },
+        "/ad": {
+          target: process.env.VITE_PUBLIC_API_URL || "http://localhost:3000",
+          changeOrigin: true,
+        },
+      },
+    },
+  });
+}
