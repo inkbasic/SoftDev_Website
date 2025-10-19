d2ee16ff24c6df339b419f010ab5674bd6c9c0b5
OkuSan - Paratpanu Pechsaman <paratpanu18@gmail.com>
2025-10-19 09:19:37 +0000

    refactor(Navbar): replace onClick navigation with href links for better accessibility

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,16 +12,16 @@ export default function Navbar() {
         
             <div className="flex flex-row items-center gap-6">
                 <div className="flex flex-row items-center gap-3">
-                    <a className="" onClick={() => navigate("/")}>Home</a>
-                    <a className="" onClick={() => navigate("/plan")}>Plan</a>
-                    <a className="" onClick={() => navigate("/dashboard")}>Dashboard</a>
-                    <a className="" onClick={() => navigate("/profile")}>Profile</a>
-                    <a className="" onClick={() => navigate("/addlocation")}>Add Location</a>
-                    <a className="" onClick={() => navigate("/save")}>Save</a>
+                    <a className="" href="/">Home</a>
+                    <a className="" href="/plan">Plan</a>
+                    <a className="" href="/dashboard">Dashboard</a>
+                    <a className="" href="/profile">Profile</a>
+                    <a className="" href="/addlocation">Add Location</a>
+                    <a className="" href="/save">Save</a>
                 </div>
                 <div className="flex flex-row items-center gap-3">
-                    <a className="" onClick={() => navigate("/login")}>Login</a>
-                    <a className="bg-primary text-white rounded-xl" onClick={() => navigate("/signup")}>Sign Up</a>
+                    <a className="" href="/login">Login</a>
+                    <a className="bg-primary text-white rounded-xl" href="/signup">Sign Up</a>
                 </div>
             </div>
 
